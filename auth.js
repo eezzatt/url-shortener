a2e7f78a17f8051776d7afa5abd8b5df6977e987
eezzatt <izzatmuhaimin10@gmail.com>
2026-06-14 16:26:59 +0800

    Added registration and login with JWT

---
+++ auth.js
@@ -0,0 +1,72 @@
+const express = require('express')
+const bcrypt = require('bcrypt')
+const pool = require('./db')
+const jwt = require('jsonwebtoken')
+require('dotenv').config()
+
+const router = express.Router()
+
+router.post('/register', async (req, res) => {
+    try {
+        const {email, password} = req.body
+
+        if (!email || !password) {
+            res.status(400).json({error: 'Email and password are required'})
+        }
+
+        const passwordHash = await bcrypt.hash(password, 10)
+
+        const result  = await pool.query (
+            'INSERT INTO users (email, password_hash) VALUES ($1, $2) RETURNING id, email',
+            [email, passwordHash]
+        )
+
+        res.status(201).json({ user: result.rows[0] })
+    }
+    catch (err) {
+        if (err.code === '23505') {
+            return res.status(409).json({ error: 'Email already registered' })
+        }
+        res.status(500).json({ error: 'Something went wrong' })
+    }
+})
+
+router.post('/login', async (req, res) => {
+    try {
+        const {email, password} = req.body
+
+        if (!email || !password) {
+            res.status(400).json({ error: 'Email and password are required'})
+        }
+
+        const result = await pool.query (
+            'SELECT * FROM users WHERE email = $1 ',
+            [email]
+        )
+
+        const user = result.rows[0]
+
+        if (!user) {
+            res.status(401).json({ error: 'Invalid email or password' })
+        }
+
+        const validPassword = await bcrypt.compare(password, user.password_hash)
+
+        if (!validPassword) {
+            res.status(401).json({ error: 'Invalid email or password' })
+        }
+
+        const token = jwt.sign(
+            { userId: user.id },
+            process.env.JWT_SECRET,
+            { expiresIn: '1h' }
+        )
+
+        res.json({ token })
+    }
+    catch (err) {
+        res.status(500).json({ error: 'Something went wrong' })
+    }
+})
+
+module.exports = router
